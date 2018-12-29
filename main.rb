80a63b0a92aee6d9985664949d5ffaf1acb45e80
Vadim Geroim <vadimgeroim.brooklyn@gmail.com>
2018-12-28 22:52:40 -0500

    initial commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative 'lib/items'
+
+puts "Программа запущена...."
+items = Items.new
+items.show_all_items
+items.ask_to_add_item
+
+puts "Сколько градусов за окном? (можно с минусом)"
+temp = gets.to_i
+items.system_decision(temp)
